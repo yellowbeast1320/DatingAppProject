33ca6c7c51289a384d5467b29f2810f3b0ea8070
Zhaoyu Zhang <zhaoyu.zhang@uconn.edu>
2020-07-14 00:17:43 -0700

    updated API

--- DatingAppApi/Controllers/ValuesController.cs
+++ DatingAppApi/Controllers/ValuesController.cs
@@ -2,27 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DatingAppApi.Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingAppApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValueController : ControllerBase
+    public class ValuesController : ControllerBase
     {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
         // GET: api/Value
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IActionResult GetValues()
         {
-            return new string[] { "value1", "value2" };
+            var values = _context.Values.ToList();
+
+            return Ok(values);
         }
 
         // GET: api/Value/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
+        [HttpGet("{id}")]
+        public IActionResult GetValue(int id)
         {
-            return "value";
+            var value = _context.Values.FirstOrDefault(x => x.Id == id);
+            return Ok(value);
         }
 
         // POST: api/Value
